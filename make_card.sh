844a75b9008739116136faa0167d36db7a4a065b
Jason Sundram <jsundram@gmail.com>
2025-01-08 16:10:36 -0500

    spacing, comments, configurable output directory, cleanup temp files

--- make_card.sh
+++ make_card.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
-# generate the explainer graphic
-uv run src/annotation.py -o output/annotation.pdf
-# create the table (front of card) and overlay the explainer
-uv run src/quartet-info-table.py -o ./output/front.pdf -a ./output/annotation.pdf -c ./colors/sashamaps.json -d ./data
-# generate the timeline
-uv run src/timeline.py -o output/timeline.pdf -c ./colors/sashamaps.json -d ./data
-# generate the graph paper and overlay the timeline on it. (-g lavender for susie)
-uv run src/graph_paper.py -t output/timeline.pdf -c ./colors/sashamaps.json -o output/back.pdf -g grey
-# merge the table (front of card) and the graph paper (back of card)
-uv run src/merge.py -f output/front.pdf -b output/back.pdf -o output/merged.pdf
-# rename the output from merged.pdf to "Haydn Info Card - Sundram - YYYY-MM-DD.pdf" 
-mv output/merged.pdf output/Haydn\ Info\ Card\ -\ Sundram\ -\ $(date +%Y-%m-%d).pdf
+OUTDIR="./output"
+
+# 1. Generate the explainer graphic.
+uv run src/annotation.py -o ${OUTDIR}/annotation.pdf
+
+# 2. Create the table (front of card) and overlay the explainer.
+uv run src/quartet-info-table.py -o ${OUTDIR}/front.pdf -a ${OUTDIR}/annotation.pdf -c ./colors/sashamaps.json -d ./data
+
+# 3. Generate the timeline.
+uv run src/timeline.py -o ${OUTDIR}/timeline.pdf -c ./colors/sashamaps.json -d ./data
+
+# 4. Generate the graph paper and overlay the timeline on it (-g lavender for susie).
+uv run src/graph_paper.py -t ${OUTDIR}/timeline.pdf -c ./colors/sashamaps.json -o ${OUTDIR}/back.pdf -g grey
+
+# 5. Merge the table (front of card) and the graph paper (back of card).
+uv run src/merge.py -f ${OUTDIR}/front.pdf -b ${OUTDIR}/back.pdf -o ${OUTDIR}/merged.pdf
+
+# 6. Rename the output from merged.pdf to "Haydn Info Card - Sundram - YYYY-MM-DD.pdf" 
+mv ${OUTDIR}/merged.pdf ${OUTDIR}/Haydn\ Info\ Card\ -\ Sundram\ -\ $(date +%Y-%m-%d).pdf
+
+# 7. Cleanup
+rm ${OUTDIR}/annotation.pdf
+rm ${OUTDIR}/timeline.pdf
+rm ${OUTDIR}/front.pdf
+rm ${OUTDIR}/back.pdf
